5dd7e977cef7324ca79e576058ca26d8770ca333
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-11-30 23:39:13 +0200

    Stupid example of signup

--- src/Application.kt
+++ src/Application.kt
@@ -9,6 +9,13 @@ import io.ktor.http.*
 import io.ktor.auth.*
 import com.fasterxml.jackson.databind.*
 import io.ktor.jackson.*
+import org.litote.kmongo.*
+
+data class User(val name: String, val phone: String, val password: String)
+data class Error(val message: String)
+
+val client = KMongo.createClient()
+val database = client.getDatabase("ktor-example-api")
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -38,6 +45,21 @@ fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        route("user") {
+            post("/signup") {
+                val user = call.receive<User>()
+                val col = database.getCollection<User>()
+                try {
+                    col.save(user)
+                    call.respond(user)
+                }
+                catch (e: Throwable) {
+                    val error = Error(e.localizedMessage)
+                    call.respond(HttpStatusCode.fromValue(501), error)
+                }
+            }
         }
 
         get("/json/jackson") {
